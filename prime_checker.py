b8e38374782485534152f107679597e5730cc578
xsnatchysquidx <51396413+xsnatchysquidx@users.noreply.github.com>
2020-10-05 16:06:48 +0300

    Update prime_checker.py
    improved the code, placed it inside a function blocked and made it more efficient by running the for loop up to the square root of the num.

--- prime_checker.py
+++ prime_checker.py
@@ -2,21 +2,34 @@
 
 num = 407
 
-# To take input from the user
-#num = int(input("Enter a number: "))
+# uncomment this to enable input from user
+#num = int(input("Please input an integer :)"))
+
 
 # prime numbers are greater than 1
-if num > 1:
-   # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
-           break
-   else:
-       print(num,"is a prime number")
-       
-# if input number is less than
-# or equal to 1, it is not prime
-else:
-   print(num,"is not a prime number")
+def is_prime(num):
+    """
+    check whether a number is prime or not.
+    we iterate over all numbers between 2 and the sqrt of num
+    (and not until num, to make it more efficient since no factor
+    of num can be greater than the sqrt of num)
+
+    :param num: number to check if its prime
+    :type num: int
+    """
+    if num > 1:
+       # check for factors
+       for i in range(2, int(num** (1/2)) + 1):
+           if (num % i) == 0:
+               print(num,"is not a prime number")
+               print(i,"times",num//i,"is",num)
+               break
+       else:
+           print(num,"is a prime number")
+
+    # if input number is less than
+    # or equal to 1, it is not prime
+    else:
+       print(num,"is not a prime number")
+
+is_prime(num)
